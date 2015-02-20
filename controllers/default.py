1b9ee23e8c525c1f06815015b018109c140e17b2
alyanshafique <alyanshafique@gmail.com>
2015-02-19 18:49:24 -0800

    updated with code from blog post

--- controllers/default.py
+++ controllers/default.py
@@ -8,6 +8,7 @@
 ## - download is for downloading files uploaded in the db (does streaming)
 ## - api is an example of Hypermedia API support and access control
 #########################################################################
+import json
 
 def index():
     """
@@ -28,9 +29,44 @@ def scores():
     return dict(message=T('Hello World'))
 
 def stats():
-    test = 'My Thumbnail'
-    response.flash = T("Welcome to web2py!")
-    return dict(message=T('Hello World'))
+    #NBA Stats API using selected player ID
+    for x in teams:
+        for y in teams[x]:
+            find_stats(y,teams[x][y])
+
+    cols = ['name','avg_defender_distance','avg_dribbles','avg_shot_distance','avg_touch_time']
+    df = pd.DataFrame(players,columns = cols)
+
+    df.head()
+
+def find_stats(name,player_id):
+    url = 'http://stats.nba.com/stats/playerdashptshotlog?'+ \
+    'DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&' + \
+    'Location=&Month=0&OpponentTeamID=0&Outcome=&Period=0&' + \
+    'PlayerID='+player_id+'&Season=2014-15&SeasonSegment=&' + \
+    'SeasonType=Regular+Season&TeamID=0&VsConference=&VsDivision='
+    
+    #Create Dict based on JSON response
+    response = requests.get(url)
+    shots = response.json()['resultSets'][0]['rowSet']
+    data = json.loads(response.text)
+    
+    #Create df from data and find averages 
+    headers = data['resultSets'][0]['headers']
+    shot_data = data['resultSets'][0]['rowSet']
+    df = pd.DataFrame(shot_data,columns=headers) 
+    avg_def = df['CLOSE_DEF_DIST'].mean(axis=1)
+    avg_dribbles = df['DRIBBLES'].mean(axis=1)
+    avg_shot_distance = df['SHOT_DIST'].mean(axis=1)
+    avg_touch_time = df['TOUCH_TIME'].mean(axis=1)
+     
+     #add averages to dictionary
+    player_stats['name'] = name
+    player_stats['avg_defender_distance']=avg_def
+    player_stats['avg_shot_distance'] = avg_shot_distance
+    player_stats['avg_touch_time'] = avg_touch_time
+    player_stats['avg_dribbles'] = avg_dribbles
+    players.append(player_stats.copy())
 
 def top_players():
     test = 'My Thumbnail'
